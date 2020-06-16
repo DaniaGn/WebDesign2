d86df1233b196f6f371cca42ebb97d998a0e0d3b
DaniaGn <61220834+DaniaGn@users.noreply.github.com>
2020-06-15 23:36:39 -0700

    Final Completed
    Added javascript message after donation click.
    improved the media queries.
    Slideshow

--- final/script.js
+++ final/script.js
@@ -2,37 +2,39 @@ document.querySelector('.hamburger-menu').addEventListener('click', () => {
     document.querySelector('.container').classList.toggle('change');
 });
 
+//--------------------SLIDESHOW START--------------
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+} 
+
+//--------------SLIDESHOW END--------------
 
 
-
-var firstName = "";
-var lastName = "";
-var email = "";
-var dType = "";
-var receipt = "";
-var anon = "";
-var list = "";
-var amount = "";
-
-$('.set-amount').autoGrow(0);
-
-/*
-	if(isiPad || jQuery.browser.mobile){
-		$('#team').hide()
-		$('.team-mobile').show();	
-	}else{
-		$('#team').show()
-		$('.team-mobile').hide();
-	}
-*/
-
-//Set & Highlight Donation Amount
-$(".button").click(function () {
-    $(".button").removeClass("selected");
-    $(this).addClass("selected");
-
-    $(this).find("input").focus();
-});
 
 //Grow the donation box if they type more than 4 numbers
 $(".set-amount").keyup(function () {
@@ -44,21 +46,10 @@ $(".set-amount").keyup(function () {
 });
 
 
-$("input").on("change", function () {
+
     // $(".donation-box").css("height", "500px");
 
-    firstName = $("#firstName").val();
-    lastName = $("#lastName").val();
-    email = $("#email").val();
 
-    if ($("#one-time").prop("checked")) {
-        dType = "One-Time";
-    }
-    if ($("#monthly").prop("checked")) {
-        dType = "Monthly";
-    }
-
-});
 
 
 document.querySelector(".scroll-btn").addEventListener("click", () => {
